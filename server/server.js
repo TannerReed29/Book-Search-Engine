1413d3e003aeaf4fc146969ff5bba70948dc0055
Tanner Brieger <tanner29@gmail.com>
2022-04-04 23:51:04 -0500

    fixed requirements on server.js

--- server/server.js
+++ server/server.js
@@ -5,23 +5,22 @@ const path = require('path');
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth');
 const db = require('./config/connection');
-const routes = require('./routes');
+//const routes = require('./routes');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const startServer = async () => {
-  const server = new ApolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
   });
-  await server.start();
-  server.applyMiddleware({ app });
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
 
-startServer()
+
+  server.applyMiddleware({ app });
+
+  
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -36,8 +35,14 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-app.use(routes);
+//app.use(routes);
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  }); 
+});
+db.on("error", (err) => {
+  console.error("MongoDB connection error: ", err);
 });
